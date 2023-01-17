90db4065363cbdc74cd836ac8bd83dc8aaf781e0
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-01-16 20:01:01 -0600

    Adding Lecture 29 files and directories

---
+++ Lecture-29-Overview/main.c
@@ -0,0 +1,19 @@
+/*
+    Author: Matt Johnson
+    Purpose: Program declares variables
+    Date: 01/16/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int matt = 44; // variable is declared and initialized
+    printf("Value is: %d", matt);
+
+    matt = 25; // variable is assigned a new value
+    printf("\nValue is: %d\n", matt);
+
+    return 0;
+}
